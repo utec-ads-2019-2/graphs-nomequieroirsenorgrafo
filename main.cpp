8aea15758534e8f6e959cd38f57dc75b180bd99d
FJ98 <felix.solano@utec.edu.pe>
2019-10-10 19:04:47 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int input;
+    while (cin >> input && input != 0) {
+        int output = input;
+
+        cout << output << endl;
+    }
+
+    return 0;
+}
+
