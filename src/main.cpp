95a8a2162290b31e15ea1aa217bd3b0017467f4f
FJ98 <felix.solano@utec.edu.pe>
2019-11-27 15:13:58 -0500

    Graph Finished

--- src/main.cpp
+++ src/main.cpp
@@ -19,30 +19,31 @@ int main() {
 
     //Tester::testdiGraphToJSON("../data/graphToJsonDir.json");
 
-    Tester::testGraphcreation();
-    Tester::testConnected();
-    Tester::testBipartite();
-//    Tester::testPrimm();
-//    Tester::testKruskal();
-//    Tester::testStronglyConnected();
-
-//    Tester::testDfs();
-//    Tester::testBellmanFord();
-//    Tester::testBellmanFordfromJSON("../data/airports2710.json");
-//    Tester::testDijkstra();
-    Tester::testDijkstrafromJSON("../data/airports2710.json");
-    Tester::testDijkstrafromJSONOnlyStart("../data/airports2710.json");
-    //Tester::testBipartiteFromJson("../data/bipartito.json");
-    //Tester::testConnectedFromJson("../data/conexo.json");
-
+//    Tester::testGraphcreation();
+//    Tester::testConnected();
+//    Tester::testBipartite();
     //Tester::testPrimm();
     //Tester::testKruskal();
     //Tester::testStronglyConnected();
-    Tester::testBellmanFordfromJSON("../data/airports2710.json");
-    Tester::testBellmanFord2();
-    Tester::testAstar();
-    Tester::testAstar_2();
-    Tester::testAstarfromJSON("../data/airports2710.json", "../data/astartsp.json");
+//    Tester::testPrimm();
+//    Tester::testKruskal();
+//    Tester::testStronglyConnected();
+//      Tester::testBipartiteFromJson("../data/bipartito.json");
+//      Tester::testConnectedFromJson("../data/conexo.json");
+
+
+//-----------------------------PROJECT-2------------------------------------
+//    Tester::testDfs();
+//    Tester::testBellmanFord();
+//    Tester::testBellmanFord2();
+//    Tester::testBellmanFordfromJSON("../data/airports2710.json");
+
+//    Tester::testAstar();
+//    Tester::testAstar_2();
+//    Tester::testAstarfromJSON("../data/airports2710.json", "../data/astartsp.json");
+//    Tester::testDijkstra();
+//    Tester::testDijkstrafromJSON("../data/airports2710.json");
+//    Tester::testDijkstrafromJSONOnlyStart("../data/airports2710.json");
 
     return 0;
 }
